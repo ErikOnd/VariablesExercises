f3d11e503c9ef13df8389c8d6e2361a8ca2857a4
Erik <erik.ondra@yahoo.com>
2022-11-28 16:23:55 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let firstName = 'Erik';
+let lastName = 'Ondra';
+let age = 25;
+let currentYear = new Date().getFullYear();
+let birthYear = currentYear - age;
+
+console.log('Hello, my name is ' + firstName + ' ' + lastName + ' ' + 'and I was born in ' + birthYear);
+
+
+
+
+
+
+
+
---
+++ index2.js
@@ -0,0 +1,31 @@
+let spaghetti = 400;
+let guanciale = 250;
+let eggYolks = 6;
+let cheese = 50;
+let blackPaper = 4;
+let waterSpoon2 = 30;
+
+// one eggYolk ~ 18g
+let eggsCheese = (eggYolks * 18) + cheese;
+
+//adding just a little paper 
+let eggsCheeseLittlePepper = eggsCheese + blackPaper / 4;
+
+//subtracting the taken amount 
+blackPaper -= -1;
+
+//cooked spaghetti weight is 1:1,5 
+spaghetti += spaghetti * 1.5;
+
+//adding pinch of salt
+spaghetti += 0.36;
+
+//adding water to the guanciale
+guanciale += waterSpoon2;
+
+let pastaGuanciale = spaghetti + guanciale;
+
+//added the toFixed function so the right number gets displayed 
+let carbonara = (pastaGuanciale + eggsCheeseLittlePepper + blackPaper).toFixed(2);
+console.log(carbonara);
+
